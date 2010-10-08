4b8c12e775ae0e04123970821dbf8e6ff70aa409
Feather.et.ELF <andelf@gmail.com>
2010-10-09 00:35:29 +0800

    modifiy normalizer, now use '-' as splitter.

--- Products/ChinaPak/zh.py
+++ Products/ChinaPak/zh.py
@@ -3,32 +3,58 @@
 #  Created     : Fri Oct  8 22:11:04 2010 by Feather.et.ELF 
 #  Copyright   : Feather Workshop (c) 2010 
 #  Description : A zope.i18n normalizer for zh 
-#  Time-stamp: <2010-10-08 23:49:30 andelf> 
+#  Time-stamp: <2010-10-09 00:29:15 andelf> 
 
 from plone.i18n.normalizer.interfaces import INormalizer
 from zope.interface import implements
-from plone.i18n.normalizer.base import mapUnicode
+from plone.i18n.normalizer.base import baseNormalize # , mapUnicode
 
 # Chinese character mapping
 from .pinyin import PinYinDict as mapping
 
+
+def mapUnicode(text, mapping=()):
+    """
+    NOET: rewrite by andelf to insert '-' between characters or english words.
+    This method is used for replacement of special characters found in a
+    mapping before baseNormalize is applied.
+    """
+    res = []
+    word = u''                          # handle english words
+    for ch in text:
+        ordinal = ord(ch)
+        if ordinal< 127:
+            word += ch
+            continue
+        elif word:
+            res.append(word)
+            word = u''
+        if ordinal in mapping:
+            res.append(mapping.get(ordinal))
+        else:
+            res.append(ch)
+    res = filter(lambda u: u and not u.isspace(), res)
+    # always apply base normalization
+    return baseNormalize(u' '.join(res))
+                                                    
+
 class Normalizer(object):
+    """
+    This normalizer can normalize any unicode string and returns a version
+    that only contains of ASCII characters.
+    
+    Let's make sure that this implementation actually fulfills the API.
+    
+    >>> from zope.interface.verify import verifyClass
+    >>> verifyClass(INormalizer, Normalizer)
+    True
+    """
+    implements(INormalizer)
+    
+    def normalize(self, text, locale=None, max_length=None):
         """
-        This normalizer can normalize any unicode string and returns a version
-        that only contains of ASCII characters.
-        
-        Let's make sure that this implementation actually fulfills the API.
-        
-        >>> from zope.interface.verify import verifyClass
-        >>> verifyClass(INormalizer, Normalizer)
-        True
-                                          """
-        implements(INormalizer)
-        
-        def normalize(self, text, locale=None, max_length=None):
-            """
-            Returns a normalized text. text has to be a unicode string.
-            """
-            return mapUnicode(text, mapping=mapping)
+        Returns a normalized text. text has to be a unicode string.
+        """
+        return mapUnicode(text, mapping=mapping)
         
 normalizer = Normalizer()
--- setup.py
+++ setup.py
@@ -1,24 +1,25 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0'
+version = '1.1'
 
 setup(name='Products.ChinaPak',
       version=version,
-      description="Chinese new translation",
+      description="Chinese new translation & normalizer",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
         "Programming Language :: Python",
         ],
-      keywords='',
-      author='',
-      author_email='',
+      keywords='plone i18n chinese normalizer translation',
+      author='andelf',
+      author_email='andelf@gmail.com',
       url='http://svn.plone.org/svn/collective/',
-      license='GPL',
+      license='GPLv2',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['Products'],
       include_package_data=True,
